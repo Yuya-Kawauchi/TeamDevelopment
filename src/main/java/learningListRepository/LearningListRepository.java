fc1d1cf952edc7ee30cb41c29565310b72072b08
河内　佑弥 <111070418+Yuya-Kawauchi@users.noreply.github.com>
2022-11-22 22:03:05 +0900

    11/22

--- src/main/java/learningListRepository/LearningListRepository.java
+++ src/main/java/learningListRepository/LearningListRepository.java
@@ -1,5 +1,11 @@
 package learningListRepository;
 
-public class LearningListRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import entity.LearningReport;
+
+@Repository
+public interface LearningListRepository
+    extends JpaRepository <LearningReport, Long> {
 }
--- src/main/java/learningListService/LearningListService.java
+++ src/main/java/learningListService/LearningListService.java
@@ -1,5 +1,27 @@
 package learningListService;
 
-public class LearningListService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import entity.LearningReport;
+import learningListRepository.LearningListRepository;
+
+@Service
+public class LearningListService {
+	 @Autowired
+	  private LearningListRepository LearningListRepository;
+	  /**
+	   * ユーザー情報 全検索
+	   * @return 検索結果
+	   */
+	  public List<LearningReport> findAll() {
+	    return LearningListRepository.findAll();
+	  }
+	  
+	  public int delete(){
+		  
+	  }
+	  
 }
