cbe304d0b0dd1129871885074eade245bc0bb360
ryosei <ryo214_007@yahoo.co.jp>
2022-12-13 23:29:41 +0900

    ログイン起動確認済

---
+++ src/main/java/com/example/demo/controller/ToppageController.java
@@ -0,0 +1,18 @@
+//
+//@Controller
+//@RequestMapping("/home")
+//public class ToppageController {
+//	 private final ToppageService service;
+//
+//	    @Autowired
+//	    public ToppageService(AccountService service) {
+//	        this.service = service;
+//	    }
+//    public String home() {
+//        return "home";
+//        /*トリビュート？
+//         * DBにSUM関数で合計を出せるのを使って月学習時間を出せるかも
+//         * 家計簿アプリを参考にできるかも
+//         * 一覧にだすのはタイムリーフとhomeタグで出来る
+//         * */
+//}
--- src/main/resources/ReportRepository.java
+++
@@ -1,4 +0,0 @@
-
-
-@Repository
-public interface findByUserId extends JpaRepository<Integer> {}
