42ead4347513202da6934c25f292c077b6e0e39e
HaruNaito <112502762+HaruNaito@users.noreply.github.com>
2022-12-13 21:44:50 +0900

    leaningnewrepository

--- src/main/java/com/example/demo/repository/chaptersRepository.java
+++ src/main/java/com/example/demo/repository/chaptersRepository.java
@@ -3,12 +3,15 @@
 //import org.springframework.data.jpa.repository.JpaRepository;
 //import org.springframework.stereotype.Repository;
 //
-//import com.example.demo.entitiy.LeaningNewReportEntity;
+//import com.example.demo.entitiy.chaptersEntity;
 //
+//
+//public class chaptersRepository {
 //
 //	/**
 //	 * ユーザー情報 Repository
 //	 */
 //	@Repository
-//	public interface LeaningNewReportRepository extends JpaRepository<LeaningNewReportEntity, Integer> {
+//	public interface LeaningNewReportRepository extends JpaRepository<chaptersEntity, Integer> {
 //	}
+//}
--- src/main/java/com/example/demo/repository/textRepository.java
+++ src/main/java/com/example/demo/repository/textRepository.java
@@ -3,10 +3,10 @@
 //import org.springframework.data.jpa.repository.JpaRepository;
 //import org.springframework.stereotype.Repository;
 //
-//import com.example.demo.entitiy.couseEntity;
+//import com.example.demo.entitiy.textsEntity;
 //
-//public class couseRepository {
+//public class textRepository {
 //	@Repository
-//	public interface LeaningNewReportRepository extends JpaRepository<couseEntity, Integer> {
+//	public interface LeaningNewReportRepository extends JpaRepository<textsEntity, Integer> {
 //	}
 //}
