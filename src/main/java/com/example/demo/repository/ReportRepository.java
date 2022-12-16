79be2fb8f0838a0d80c5ff1c07b91d823d5493a5
kai1120s <kai20s.cats@gmail.com>
2022-12-17 01:24:12 +0900

    12/17 1:23

--- src/main/java/com/example/demo/repository/ReportRepository.java
+++ src/main/java/com/example/demo/repository/ReportRepository.java
@@ -1,4 +1,5 @@
-//
-//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+
 //@Repository
-//public interface findByUserId extends JpaRepository<Integer> {}
+//public interface ReportRepository extends JpaRepository<Integer> {}
