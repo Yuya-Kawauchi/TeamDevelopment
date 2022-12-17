729229424a216b51d2768528d00b2c33e80deb7f
HaruNaito <112502762+HaruNaito@users.noreply.github.com>
2022-12-17 17:03:03 +0900

    learningnewreport

--- src/main/java/com/example/demo/entity/LearningNewReportEntity.java
+++ src/main/java/com/example/demo/entity/LearningNewReportEntity.java
@@ -1,41 +1,52 @@
 package com.example.demo.entity;
+import java.sql.Time;
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
 
 @Entity
 @Data
-@Table(name = "user")
-public class LearningNewReportEntity {
-	   /**
-	   * ID
-	   */
-	  @Column(name = "created_at")
-	  private Integer created_at;
-	  /**
-	   * 開始時間
-	   */
-	  @Column(name = "start_time")
-	  private String start_time;
-	  /**
-	   *終了時間
-	   */
-	  @Column(name = "end_time")
-	  private String end_time;
-	  /**
-	   * 学習登録
-	   */
-	  @Column(name = "course_id")
-	  private String course_id;
-	  
-	  @Column(name = "chapters_id")
-	  private String chapters_id;
-	  
-	  @Column(name = "text_id")
-	  private String text_id;
-	  
-	 
-	}
+@Table(name = "report")
+public class LearningNewReportEntity{
+ 
+	
+ @Id
+ @NotNull
+ @Column(name = "rema")
+ private Integer rema;
+ 
+ @NotNull
+ @Column(name = "user_id")
+ private Integer user_id;
+
+ @NotNull
+ @Column(name = "created_at")
+ @DateTimeFormat(pattern = "YYYY-MM-DD")
+ private Date created_at;
+ 
+ @NotNull
+ @DateTimeFormat(pattern = "HH:mm:ss")
+ @Column(name = "start_time")
+ private Time start_time ;
+ 
+ @NotNull
+ @DateTimeFormat(pattern = "HH:mm:ss")
+ @Column(name = "end_time")
+ private Time end_time ;
+ 
+ @NotNull
+ @Column(name = "course_id")
+ private String course_id;
+ 
+ @Column(name = "remark")
+ private String remark;
+}
