0199d5b989bb97997efc8e9ded8aa9cfbcf97cda
kai1120s <kai20s.cats@gmail.com>
2022-12-20 00:18:54 +0900

    12/20 0:18

--- src/main/java/com/example/demo/controller/ToppageController.java
+++ src/main/java/com/example/demo/controller/ToppageController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.entity.UserEntity;
 import com.example.demo.service.ToppageService;
@@ -23,9 +26,16 @@ public class ToppageController {
 		 model.addAttribute("userlist", userlist);
 		return "topPage";
 		
-		
-	
 	}
+	
+	  @PostMapping(path = "home", params = "edit")
+	  public String postLearningList(RedirectAttributes redirectAttributes,@RequestParam("user_id") Integer user_id)  {
+		 
+		  
+		  redirectAttributes.addFlashAttribute("user_id", user_id);
+
+		  return "redirect:/userlist/{user_id}";
+	  }
 //	@Autowired
 //	private LearningReport learningReport;
 //	ResultSet date = learningReport.executeQuery("SELECT created_at FROM report_tb;");
