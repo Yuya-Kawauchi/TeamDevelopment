46cfe006b78bc0b9c50ca9a2db4df00b17dced4b
HaruNaito <112502762+HaruNaito@users.noreply.github.com>
2022-12-22 01:46:02 +0900

    learninnewreportservice
    総学習時間の値を実装完了

--- src/main/java/com/example/demo/service/LearningNewReportService.java
+++ src/main/java/com/example/demo/service/LearningNewReportService.java
@@ -1,7 +1,10 @@
 package com.example.demo.service;
 
-
+import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,35 +16,39 @@ import com.example.demo.repository.CourseRepository;
 import com.example.demo.repository.LearningNewReportRepository;
 import com.example.demo.repository.TextRepository;
 
+@Service
+public class LearningNewReportService {
+	@Autowired
+	LearningNewReportRepository learningNewReportRepository;
 
-	@Service
-	public class LearningNewReportService{
-			@Autowired
-			LearningNewReportRepository learningNewReportRepository;
-			
-			@Autowired
-			ChapterRepository chapterrepository;
-			
-			@Autowired
-			CourseRepository courserepository;
-			
-			@Autowired
-			TextRepository textrepository;
+	@Autowired
+	ChapterRepository chapterrepository;
 
-			public void insert(LearningNewReportForm LearningNewReportForm) {
-			
-				LearningNewReportEntity user = new LearningNewReportEntity();
-				user.setRema(LearningNewReportForm.getRema());
-//				user.setUser_id(LearningNewReportForm.getUser_id());
-				user.setUser_id(1);
-			    user.setCreated_at(LearningNewReportForm.getCreated_at());
-			    user.setStart_time(LocalTime.parse(LearningNewReportForm.getStart_time().substring(0,5)+":00"));
-			    user.setEnd_time(LocalTime.parse(LearningNewReportForm.getEnd_time().substring(0,5)+":00"));
-			    user.setCourse_id(LearningNewReportForm.getCourse_id());
-			    user.setRemark(LearningNewReportForm.getRemark());				
-				learningNewReportRepository.save(user);
-			}
-		}
+	@Autowired
+	CourseRepository courserepository;
+
+	@Autowired
+	TextRepository textrepository;
+
+	public void insert(LearningNewReportForm LearningNewReportForm) {
+  
+		LearningNewReportEntity user = new LearningNewReportEntity();
+		user.setRema(LearningNewReportForm.getRema());
+		//				user.setUser_id(LearningNewReportForm.getUser_id());
+		user.setUser_id(1);
+		user.setCreated_at(LearningNewReportForm.getCreated_at());
+		user.setStart_time(LocalTime.parse(LearningNewReportForm.getStart_time().substring(0, 5) + ":00"));
+		user.setEnd_time(LocalTime.parse(LearningNewReportForm.getEnd_time().substring(0, 5) + ":00"));
+		user.setCourse_id(LearningNewReportForm.getCourse_id());
+		user.setRemark(LearningNewReportForm.getRemark());
+		Long millis = ChronoUnit.MILLIS.between(user.getEnd_time(), user.getStart_time());
+		Time total = new Time(millis);
+        String str = new SimpleDateFormat("HH:mm:ss").format(total);
+		LocalTime timeT = LocalTime.parse(str, DateTimeFormatter.ofPattern("HH:mm:ss"));
+		user.setTotal_time(timeT);
+		learningNewReportRepository.save(user);
+	}
+}
 //	    @Autowired 
 //	    LearningNewReportRepository leaningNewReportRepository ;		
 //		public List<LearningNewReportEntity> serchAll() {
@@ -70,4 +77,3 @@ import com.example.demo.repository.TextRepository;
 //			user.setTextId(reportForm.getText_id());
 //			LearningNewReportRepository.save(user);
 //			}
-	
