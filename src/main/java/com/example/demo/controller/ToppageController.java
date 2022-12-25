e68d1c9cb7e1c51ccb44518863ee5fe45a0a2ca8
ryosei <ryo214_007@yahoo.co.jp>
2022-12-25 14:49:43 +0900

    userId取得

--- src/main/java/com/example/demo/controller/ToppageController.java
+++ src/main/java/com/example/demo/controller/ToppageController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,21 +13,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.entity.UserEntity;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.ToppageService;
 
 @Controller
 public class ToppageController {
 	
-	
+	@Autowired
+	private UserRepository userRepository;
 	@Autowired
 	private ToppageService toppageService;
 	 
 	@GetMapping("/home")
 	public String getHome(Model model) {
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		//Principalからログインユーザの情報を取得
+		String userName = auth.getName();
+		
+		UserEntity enteity = new UserEntity();
 		 List<UserEntity> userlist = toppageService.searchAll();
 		 model.addAttribute("userlist", userlist);
+		 
+			
+			
+		 UserEntity user= new UserEntity();
+		 for (int i = 0; i < userlist.size(); i++) {
+			 if(userlist.get(i).getName().equals(userName));
+			 user.setUser_id(userlist.get(i).getUser_id());
+		 }
+		 model.addAttribute("user_id", user.getUser_id());
+	
+			 
+		 
 		return "topPage";
-		
 	}
 	
 	  @PostMapping(path = "home", params = "edit")
