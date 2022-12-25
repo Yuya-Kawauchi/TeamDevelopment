9148e5b66515eed8ab1e64c444729b3ace48c5b8
ryosei <ryo214_007@yahoo.co.jp>
2022-12-25 18:40:16 +0900

    ユーザー新規登録動作確認OK(南)

--- src/main/java/com/example/demo/dto/UserRequest.java
+++ src/main/java/com/example/demo/dto/UserRequest.java
@@ -20,7 +20,7 @@ public class UserRequest implements Serializable {
   /**
    * 名前
    */
-  @NotNull(message = "名前を入力してください")
+  @NotNull(message = "IDを入力してください")
   private Integer user_id;
   
   @NotEmpty(message = "名前を入力してください")
@@ -29,11 +29,11 @@ public class UserRequest implements Serializable {
   
   private String name_kana;
  
-  @NotEmpty(message = "名前を入力してください")
+  @NotEmpty(message = "パスワードを入力してください")
   @Size(max = 20, message = "パスワードは20文字以内で入力してください")
   private String password;
   
-  @NotEmpty(message = "名前を入力してください")
+  @NotEmpty(message = "メールアドレスを入力してください")
   @Size(max = 50, message = "アドレスは50文字以内で入力してください")
   private String mail_address;
 }
